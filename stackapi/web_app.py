3bc24edc8a92caa2e7f165471b0bbc5830b3517c
abhish312 <33264211+abhish312@users.noreply.github.com>
2018-10-31 22:04:51 +0530

    Update web_app.py

--- stackapi/web_app.py
+++ stackapi/web_app.py
@@ -4,11 +4,9 @@ from app.Scraper import start
 app = Flask(__name__)
 app.secret_key = 'dopamin'
 
-
 @app.route('/')
 def my_form():
     return render_template("index.html")
-
 
 @app.route('/api/<question>', methods=['GET', 'POST'])
 def home(question):
@@ -16,7 +14,6 @@ def home(question):
         question_api = start(question)
 
         return jsonify(question_api)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
